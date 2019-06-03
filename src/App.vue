b116505b42d649f69a640ad1ee9222d790eb3b89
Ayo-Oluwa Adebayo <ayo-oluwaadebayo@Ayo-Oluwas-MacBook-Pro.local>
2019-06-03 09:25:22 +0100

    Setting up VueJs

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <product-list-one></product-list-one>
+    <ProductListTwo></ProductListTwo>
+  </div>
+</template>
+
+<script>
+import ProductListOne from './components/ProductListOne.vue';
+import ProductListTwo from './components/ProductListTwo.vue';
+
+export default {
+  components: {
+    'product-list-one': ProductListOne,
+    ProductListTwo
+  },
+  name: 'app',
+  data () {
+    return {
+      // products: []
+    }
+  }
+}
+</script>
+
+
+<style>
+  body {
+    font-family: Ubuntu;
+    color: #555;
+  }
+</style>
